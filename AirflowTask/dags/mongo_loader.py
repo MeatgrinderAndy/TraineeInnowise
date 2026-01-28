250015e8aadf1c61cff8648a26acbfbd0c83ab75
Andrey Kantarovich <andreykantarovich@gmail.com>
2026-01-28 13:08:05 +0300

    Mongo uploader fixed

--- AirflowTask/dags/mongo_loader.py
+++ AirflowTask/dags/mongo_loader.py
@@ -1,11 +1,13 @@
 import os
-import json
+import logging
 import pandas as pd
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.mongo.hooks.mongo import MongoHook
 from airflow.datasets import Dataset
 from airflow.utils.dates import days_ago
+
+logger = logging.getLogger(__name__)
 
 DATA_FOLDER = os.getenv("DATA_FOLDER")
 OUTPUT_FILE = os.getenv("OUTPUT_FILENAME")
@@ -18,25 +20,31 @@ default_args = {
 }
 
 def upload_to_mongo():
+    logger.info(f"Uploading to MongoDB. Reading file: {FULL_PATH}")
+    
     try:
-        df = pd.read_csv(FULL_PATH)
+        df = pd.read_csv(FULL_PATH, keep_default_na=False, na_values=[])
     except FileNotFoundError:
-        print("File not found.")
-        return
-
+        logger.error(f"File not found at path: {FULL_PATH}. Stopping task.")
+        raise
+    
     records = df.to_dict(orient='records')
 
-    hook = MongoHook(conn_id='mongo_default')
-    
     try:
-        hook.delete_many(mongo_collection='comments', filter_doc={}, mongo_db='airflow_db')
-        print("Old data cleared.")
+        hook = MongoHook(conn_id='mongo_default')
+        
+        try:
+            hook.delete_many(mongo_collection='comments', filter_doc={}, mongo_db='airflow_db')
+            logger.info("Old data cleared successfully.")
+        except Exception as e:
+            logger.warning(f"Could not clear collection. Еmpty or first run: {e}")
+        
+        hook.insert_many(mongo_collection='comments', docs=records, mongo_db='airflow_db')
+        logger.info(f"Success. Inserted {len(records)} records into MongoDB.")
+        
     except Exception as e:
-        print(f"Collection might be empty or not exists: {e}")
-    
-    hook.insert_many(mongo_collection='comments', docs=records, mongo_db='airflow_db')
-    print(f"Inserted {len(records)} records into MongoDB.")
-
+        logger.error(f"Failed to connect or insert into MongoDB: {e}")
+        raise e 
 with DAG(
     dag_id='2_mongo_loader_dag',
     default_args=default_args,
@@ -45,6 +53,6 @@ with DAG(
 ) as dag:
 
     load_task = PythonOperator(
-        task_id='load_to_mongodb',
+        task_id='upload_to_mongo',
         python_callable=upload_to_mongo
     )
